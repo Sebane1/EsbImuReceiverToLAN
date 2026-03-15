06b65e735b94781fcbe5fd4d8b811c2eb7cc4884
Sebastina <sebane1@thavea.com>
2026-03-15 00:44:02 -0500

    UF2 Firmware creator.

--- EsbImuReceiverToLanESP32/listen_logs.py
+++ EsbImuReceiverToLanESP32/listen_logs.py
@@ -1,15 +1,31 @@
 import socket
 import traceback
-
 import sys
+import threading
+import time
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 6970
+ESP32_PORT = 6971
+
+def send_wakeup(sock):
+    while True:
+        try:
+            sock.sendto(b"ENABLE_LOGS", ("255.255.255.255", ESP32_PORT))
+        except:
+            pass
+        time.sleep(5)
 
 try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.bind((UDP_IP, UDP_PORT))
+    
+    # Start wakeup thread
+    t = threading.Thread(target=send_wakeup, args=(sock,))
+    t.daemon = True
+    t.start()
     
     print("Listening for ESP32 logs on UDP port " + str(UDP_PORT) + "...")
     
